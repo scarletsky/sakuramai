a37b16c65f710861d2471347cd841743ce95d687
scarletsky <scarletsky1025@gmail.com>
2014-03-04 22:03:40 +0800

    chore: remove all files

--- index.wsgi
+++
@@ -1,8 +0,0 @@
-import os 
-import django.core.handlers.wsgi
-
-import sae
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'sakuramai.settings' 
-
-application = sae.create_wsgi_app(django.core.handlers.wsgi.WSGIHandler())
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-from django.core.handlers.wsgi import WSGIHandler
-sys.path.append("/home/scarlex/sae/sakuramai/1")
-os.environ["DJANGO_SETTINGS_MODULE"] = "sakuramai.settings"
-application = WSGIHandler()
