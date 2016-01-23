782d6119fe94dc680bff8c2584e4e3d68e260c1d
scarletsky <scarletsky1025@gmail.com>
2016-01-23 22:48:09 +0800

    Add signup form modal

--- white/static/2016/scripts/main.js
+++ white/static/2016/scripts/main.js
@@ -1,4 +1,5 @@
 (function ($) {
+
 $(document).ready(function() {
 
   $('[data-dna]').mouseover(function(e) {
@@ -6,6 +7,24 @@ $(document).ready(function() {
     var dna = $elem.data('dna');
     $('#dna').attr('class', 'p' + dna);
   });
+
+  $('#form-success.modal')
+    .modal('attach events', '#team-form.modal #team-submit.button');
+
+  $('#click-me-to-signup').click(function(e) {
+    e.preventDefault();
+    $('#team-form.modal').modal('show');
+  });
+
+  // $('#single-link').click(function(e) {
+  //   e.preventDefault();
+  //   $('#single-form').modal('show');
+  // });
+
+  // $('#team-link').click(function(e) {
+  //   e.preventDefault();
+  //   $('#team-form').modal('show');
+  // });
 
 });
 } (jQuery));
