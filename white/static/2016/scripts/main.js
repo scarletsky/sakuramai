a051b564149a305069a33932e2a85a24dd4d5a33
scarletsky <scarletsky1025@gmail.com>
2016-01-24 13:21:47 +0800

    Adjust indent size

--- white/static/2016/scripts/main.js
+++ white/static/2016/scripts/main.js
@@ -1,33 +1,33 @@
-(function ($) {
+(function($) {
 
-$(document).ready(function() {
+    $(document).ready(function() {
 
-  $('[data-dna]').mouseover(function(e) {
-    var $elem = $(this);
-    var dna = $elem.data('dna');
-    $('#dna').attr('class', 'p' + dna);
-  });
+        $('[data-dna]').mouseover(function(e) {
+            var $elem = $(this);
+            var dna = $elem.data('dna');
+            $('#dna').attr('class', 'p' + dna);
+        });
 
-  $('#form-success.modal')
-    .modal('attach events', '#team-form.modal #team-submit.button');
+        $('#form-success.modal')
+            .modal('attach events', '#team-form.modal #team-submit.button');
 
-  $('#form-fail.modal')
-    .modal('attach events', '#single-form.modal #single-submit.button');
+        $('#form-fail.modal')
+            .modal('attach events', '#single-form.modal #single-submit.button');
 
-  $('#click-me-to-signup').click(function(e) {
-    e.preventDefault();
-    $('#team-form.modal').modal('show');
-  });
+        $('#click-me-to-signup').click(function(e) {
+            e.preventDefault();
+            $('#team-form.modal').modal('show');
+        });
 
-  $('#single-link').click(function(e) {
-    e.preventDefault();
-    $('#single-form.modal').modal('show');
-  });
+        $('#single-link').click(function(e) {
+            e.preventDefault();
+            $('#single-form.modal').modal('show');
+        });
 
-  $('#team-link').click(function(e) {
-    e.preventDefault();
-    $('#team-form.modal').modal('show');
-  });
+        $('#team-link').click(function(e) {
+            e.preventDefault();
+            $('#team-form.modal').modal('show');
+        });
 
-});
-} (jQuery));
+    });
+}(jQuery));
